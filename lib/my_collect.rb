332d0899e15cdbc8b413b4461ff112b1d42c9fc9
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-18 07:11:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,16 @@
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.count
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
+
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 my_collect(array) do |name|
   name.split(" ").first
-end
+end 
